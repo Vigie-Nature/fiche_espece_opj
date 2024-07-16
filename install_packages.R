3ef5ed3de440317977225c346a84710ed324a0af
mael.pretet <mael.pretet1@mnhn.fr>
2024-07-16 16:29:28 +0200

    refactor: génération du quarto via la pipeline et non via le script R

---
+++ install_packages.R
@@ -0,0 +1,4 @@
+install.packages(c('cowplot', 'dplyr', 'echarts4r', 'ggimage', 'ggplot2',
+                   'gridExtra', 'here', 'knitr', 'lubridate', 'magick', 'plotly',
+                   'quarto', 'RMySQL', 'rlang', 'scales', 'sf', 'stringr', 'tibble',
+                   'tidyr'), repos='http://cran.rstudio.com/')
