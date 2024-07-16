13501924349d4af35a5474534ab5b29cc8b99fe4
mael.pretet <mael.pretet1@mnhn.fr>
2024-07-16 16:19:03 +0200

    debug: ajout trycatch

--- programs/boucle_quarto.R
+++ programs/boucle_quarto.R
@@ -1,5 +1,3 @@
-print("Flag 1")
-
 # Librairies
 library(dplyr)
 # library(fs)
@@ -7,14 +5,10 @@ library(here)
 library(quarto)
 library(stringr)
 
-print("Flag 2")
-
 # Fonctions
 readRenviron(".env")
 source("fonctions/function_import_from_mosaic.R")
 # source("programs/generate_html.R")
-
-print("Flag 3")
 
 # Départements avec numéro et région
 reg_dep = read.csv2("data/departements-france.csv", sep=",")
@@ -27,8 +21,6 @@ if (Sys.getenv("CI") == "TRUE") {
                         "Citrons", "Paon du jour", "Piérides blanches", "Lycènes bleus","Tabac d'Espagne", "Gazé")
 }
 
-print("Flag 4")
-
 # liste_principale <- c("Amaryllis", "Argus verts", "Aurores", "Belle-dame",
 #                       "Brun des pélargoniums", "Citrons", "Cuivré",
 #                       "Demi-deuils", "Flambés", "Gazé", "Hespérides orangées",
@@ -38,17 +30,28 @@ print("Flag 4")
 #                       "Robert-le-diable", "Silène", "Souci", "Sylvains",
 #                       "Tabac d'Espagne", "Tircis", "Vulcain")
 
-# Data frame des espèces
-df_sp_for_names = import_from_mosaic(query = read_sql_query("SQL/export_a_plat_OPJ.sql"),
-                               database_name = "spgp") %>%
-  filter(!is.na(dept_code),         # suppression des départements nuls
-         str_length(dept_code)==2,  # suppression des drom-com
-         annee >= 2019,
-         nom_espece %in% liste_principale) %>%         # suppression des données avant 2018
-  mutate(an_sem = if_else(as.numeric(num_semaine) < 10,
-                          paste0(annee, "-S0", num_semaine),
-                          paste0(annee, "-S", num_semaine)) ) %>%
-  left_join(reg_dep, by = c("dept_code" = "code_departement")) # ajout des départements
+tryCatch({
+  message("Début lecture base")
+  
+  # Data frame des espèces
+  df_sp_for_names = import_from_mosaic(query = read_sql_query("SQL/export_a_plat_OPJ.sql"),
+                                       database_name = "spgp") %>%
+    filter(!is.na(dept_code),         # suppression des départements nuls
+           str_length(dept_code)==2,  # suppression des drom-com
+           annee >= 2019,
+           nom_espece %in% liste_principale) %>%         # suppression des données avant 2018
+    mutate(an_sem = if_else(as.numeric(num_semaine) < 10,
+                            paste0(annee, "-S0", num_semaine),
+                            paste0(annee, "-S", num_semaine)) ) %>%
+    left_join(reg_dep, by = c("dept_code" = "code_departement")) # ajout des départements
+  
+  message("Fin lecture base")
+}, error = function(e) {
+  message("Erreur : ", e$message)
+  cli::cli_abort("Erreur détectée : {e$message}")
+})
+  
+
 
 
 # time = Sys.time()
@@ -65,21 +68,26 @@ df_sp_for_names = import_from_mosaic(query = read_sql_query("SQL/export_a_plat_O
 # }
 # print(Sys.time() - time)
 
-
-time = Sys.time()
-# Boucle sur les noms d'espèces
-for (sp_name in unique(df_sp_for_names$nom_espece)) {
-  filename = paste0("maquette_espece_", sp_name, ".html")
-  quarto_render(input = "maquette_espece.qmd",
-                execute_params = list("sp_name" = sp_name),
-                output_file = filename)
-  
-  file.copy(from = filename,
-            to = paste0("out/", filename), overwrite = TRUE)
-  file.remove(filename)
-}
-print(Sys.time() - time)
-
+tryCatch({
+  message("Début boucle")
+  time = Sys.time()
+  # Boucle sur les noms d'espèces
+  for (sp_name in unique(df_sp_for_names$nom_espece)) {
+    filename = paste0("maquette_espece_", sp_name, ".html")
+    quarto_render(input = "maquette_espece.qmd",
+                  execute_params = list("sp_name" = sp_name),
+                  output_file = filename)
+    
+    file.copy(from = filename,
+              to = paste0("out/", filename), overwrite = TRUE)
+    file.remove(filename)
+  }
+  print(Sys.time() - time)
+  message("Fin boucle")
+}, error = function(e) {
+  message("Erreur : ", e$message)
+  cli::cli_abort("Erreur détectée : {e$message}")
+})
 # time = Sys.time()
 # # Boucle sur les noms d'espèces
 # for (sp_name in unique(df_sp_for_names$nom_espece)) {
