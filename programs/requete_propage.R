410c9b7c890d529330382b781ef3966a677fdea9
mael.pretet <mael.pretet1@mnhn.fr>
2024-07-23 11:43:29 +0200

    feat: ajout de sauvegarde + visualisation basique pour df_propage

--- programs/requete_propage.R
+++ programs/requete_propage.R
@@ -2,5 +2,12 @@
 df_propage = import_from_mosaic(query = read_sql_query("SQL/export_a_plat_propage.sql"),
                                 database_name = "espaces_verts")
 
-df_propage = import_from_mosaic(query = read_sql_query("../../../Téléchargements/export_a_plat_propage.sql"),
-                                database_name = "espaces_verts")
+saveRDS(object = df_propage, file = "data/rdata/df_propage.rds")
+
+boxplot(as.numeric(difftime(strptime(df_propage$heure_fin, format = "%H:%M:%S"),
+                            strptime(df_propage$heure_debut, format = "%H:%M:%S"),
+                            units = "mins")))
+
+
+write.csv2(df_propage, file = "data/rdata/df_propage.csv",
+           fileEncoding = "utf-8", row.names = FALSE, col.names = TRUE)
