79a4b44e9432462dd072477b102bae97da753680
mael.pretet <mael.pretet1@mnhn.fr>
2024-07-25 16:57:34 +0200

    fix: on essaye d'enregistrer les artéfacts

--- programs/boucle_quarto.R
+++ programs/boucle_quarto.R
@@ -14,20 +14,20 @@ source("fonctions/function_import_from_mosaic.R")
 reg_dep = read.csv2("data/departements-france.csv", sep=",")
 
 #liste principale des papillons de l'observatoire
-if (Sys.getenv("CI") == "TRUE") {
-  liste_principale <- c("Amaryllis", "Argus verts", "Belle-dame",
-                        "Citrons", "Paon du jour", "Piérides blanches", "Lycènes bleus","Tabac d'Espagne", "Gazé")
+if (Sys.getenv("CI") == "true") {
+  liste_principale <- c("Amaryllis", "Argus verts", "Belle-dame", "Citrons",
+                        "Paon du jour", "Piérides blanches", "Tabac d'Espagne", "Gazé")
 }else{
-  # liste_principale <- c("Amaryllis", "Argus verts", "Belle-dame",
-  #                       "Citrons", "Paon du jour", "Piérides blanches", "Lycènes bleus","Tabac d'Espagne", "Gazé")
-  liste_principale <- c("Amaryllis", "Argus verts", "Aurores", "Belle-dame",
-                        "Brun des pélargoniums", "Citrons", "Cuivré",
-                        "Demi-deuils", "Flambés", "Gazé", "Hespérides orangées",
-                        "Hespérides tachetées", "Lycènes bleus", "Machaons",
-                        "Mégères", "Moro-sphinx", "Myrtil", "Paon du jour",
-                        "Petites tortues", "Piérides blanches", "Procris",
-                        "Robert-le-diable", "Silène", "Souci", "Sylvains",
-                        "Tabac d'Espagne", "Tircis", "Vulcain")
+  liste_principale <- c("Amaryllis", "Argus verts", "Belle-dame", "Citrons",
+                        "Paon du jour", "Piérides blanches", "Tabac d'Espagne", "Gazé")
+  # liste_principale <- c("Amaryllis", "Argus verts", "Aurores", "Belle-dame",
+  #                       "Brun des pélargoniums", "Citrons", "Cuivré",
+  #                       "Demi-deuils", "Flambés", "Gazé", "Hespérides orangées",
+  #                       "Hespérides tachetées", "Lycènes bleus", "Machaons",
+  #                       "Mégères", "Moro-sphinx", "Myrtil", "Paon du jour",
+  #                       "Petites tortues", "Piérides blanches", "Procris",
+  #                       "Robert-le-diable", "Silène", "Souci", "Sylvains",
+  #                       "Tabac d'Espagne", "Tircis", "Vulcain")
 }
 
 tryCatch({
