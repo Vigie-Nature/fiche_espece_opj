3c982001699a957b292a49b022a63c583dad267c
mael.pretet <mael.pretet1@mnhn.fr>
2025-11-19 16:02:20 +0100

    fix: add flag

--- fonctions/library.R
+++ fonctions/library.R
@@ -1,21 +1,28 @@
 # R 4.4.2
+print("flag1")
 suppressWarnings(suppressMessages(library(cowplot))) # 1.1.3
 suppressWarnings(suppressMessages(library(dplyr))) # 1.1.4
 suppressWarnings(suppressMessages(library(echarts4r))) # 0.4.5
+print("flag2")
 #suppressWarnings(suppressMessages(library(ggimage))) # 0.3.3
 suppressWarnings(suppressMessages(library(ggplot2))) # 3.5.1 
 suppressWarnings(suppressMessages(library(gridExtra))) # 2.3
 library(here) # 1.0.1
+print("flag3")
 suppressWarnings(suppressMessages(library(httr))) # 1.4.7
 library(ISOweek)
 library(knitr) # 1.49
+print("flag4")
 suppressWarnings(suppressMessages(library(lubridate))) # 1.9.3
 suppressWarnings(suppressMessages(library(magick))) # 2.8.5
 suppressWarnings(suppressMessages(library(plotly))) # 4.10.4
+print("flag5")
 suppressWarnings(suppressMessages(library(RMySQL))) # 0.10.29
 library(rlang) # 1.1.4
 library(scales) # 1.3.0
+print("flag6")
 suppressWarnings(suppressMessages(library(sf))) # 1.0-19
 library(stringr) # 1.5.1
 library(tibble) # 3.2.1
+print("flag7")
 library(tidyr) # 1.3.1
