5b863fcf9e2f72a79ea32648b03c8598361d7039
mael.pretet <mael.pretet1@mnhn.fr>
2026-03-18 17:40:56 +0100

    refactor: ajout de boucle

--- programs/boucle_quarto.R
+++ programs/boucle_quarto.R
@@ -48,5 +48,23 @@ for (sp_name in liste_principale) {
   
 }
 
+# Boucle sur les noms d'espèces
+for (sp_name in liste_principale) {
+  
+  tryCatch({
+    filename = paste0("maquette_espece_page_", sp_name, ".html")
+    
+    quarto_render(input = "maquette_espece_page.qmd",
+                  execute_params = list("sp_name" = sp_name),
+                  output_file = filename)
+    
+    file.rename(filename, file.path("docs", filename))
+    
+  }, error = function(e) {
+    message(sprintf("Error rendering document_%s.qmd: %s", sp_name, e$message))
+  })
+  
+}
+
 print(Sys.time() - time)
 
